d05edaebdd25c8bb96bb1b847b1b5354cfacf472
CarlosFuSiCong <sicong.fu@outlook.com>
2025-05-15 12:11:25 +0930

    add: linked project item to css

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 import "./index.css"
+
+import{
+    navbar, navbar_container, navbar_container_logo, navbar_container_searchBar, navbar_container_create, navbar_container_create_img
+} from '@/js/common.js'
+
---
+++ src/js/common.js
@@ -0,0 +1,17 @@
+import data from '/data/db.json'
+
+/* --- Navbar --- */
+export const navbar = document.querySelector("nav");
+export const navbar_container = navbar.querySelector(".container");
+export const navbar_container_logo = navbar_container.querySelector('.logo');
+export const navbar_container_searchBar = navbar_container.querySelector(".search_bar")
+export const navbar_container_create = navbar_container.querySelector(".create")
+export const navbar_container_create_img = navbar_container.querySelector("img")
+
+// /* --- Main --- */
+// export const main = document.querySelector("main");
+// export const main_container = main.querySelector(".container");
+
+// /* --- Main Left --- */
+// export const left = main_container.querySelector('.main-left');
+// export const left_profile = new Profile(data.profile).dom;
