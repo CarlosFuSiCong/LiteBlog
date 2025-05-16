645e4a0abf142f0b7dfd56d817332e9e8e079ebf
CarlosFuSiCong <sicong.fu@outlook.com>
2025-05-16 14:55:16 +0930

    upd: main left create

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import "./index.css"
+import { profile } from './data/db.json'
 
-import{
-    navbar, navbar_container, navbar_container_logo, navbar_container_searchBar, navbar_container_create, navbar_container_create_img
+import {
+  navbar_container_logo,
+  navbar_container_create_img,
+  // left,
+  // left_profile,
+  // left_sidebar,
+  // left_sidebar_post
 } from '@/js/common.js'
 
+navbar_container_logo.textContent = "LiteBlog"
+navbar_container_create_img.src = profile.img_src
+
+// left.appendChild(left_profile)
+// left.appendChild(left_sidebar)
+// left.appendChild(left_sidebar_post)
--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,22 @@
 import data from '/data/db.json'
+// import { Profile } from '@/js/components/mainLeft/profile';
+// import { Sidebar } from '@/js/components/mainLeft/sidebar';
+// import { create_post } from '@/js/components/mainLeft/createPost';
 
 /* --- Navbar --- */
 export const navbar = document.querySelector("nav");
 export const navbar_container = navbar.querySelector(".container");
 export const navbar_container_logo = navbar_container.querySelector('.logo');
-export const navbar_container_searchBar = navbar_container.querySelector(".search_bar")
+export const navbar_container_searchBar = navbar_container.querySelector(".search-bar")
 export const navbar_container_create = navbar_container.querySelector(".create")
 export const navbar_container_create_img = navbar_container.querySelector("img")
 
-// /* --- Main --- */
-// export const main = document.querySelector("main");
-// export const main_container = main.querySelector(".container");
+/* --- Main --- */
+export const main = document.querySelector("main");
+export const main_container = main.querySelector(".container");
 
-// /* --- Main Left --- */
-// export const left = main_container.querySelector('.main-left');
-// export const left_profile = new Profile(data.profile).dom;
+/* --- Main Left --- */
+export const left = main_container.querySelector('.main-left');
+export const left_profile = new Profile(data.profile).dom;
+export const left_sidebar = new Sidebar(data.sidebar).dom;
+export const left_sidebar_post = create_post()
